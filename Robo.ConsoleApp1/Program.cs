6c37b9bd5029ded693f2e1d7547060caa9d3db83
tiago <tiagoeweber@gmail.com>
2025-04-16 20:48:22 -0300

    Criado o meio para que ele consiga se mexer para direita, esquerda

--- Robo.ConsoleApp1/Program.cs
+++ Robo.ConsoleApp1/Program.cs
@@ -4,11 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("Robô Tupiniquim");
-            Console.WriteLine("---------------------------------------");
+            int posicaoX = 0;
+            int posicaoY = 0;
 
-            Console.ReadLine();
+            string localizacaoRobo = $"{posicaoX},{posicaoY}";
+            char direcaoRobo = 'L'; //Direção começa
+            string instrucao = "eee".ToUpper();
+
+            while (true)
+            {
+                for (int i = 0; i < instrucao.Length; i++)
+                {
+                    char comando = instrucao[i];
+
+                    if (comando == 'D')
+                    {
+                        direcaoRobo = VirarDireita(direcaoRobo);
+                    }
+                    else if (comando == 'E')
+                    {
+                        direcaoRobo = VirarEsquerda(direcaoRobo);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Comando inválido: Utilize [D], para direita, [M], para mover, ou [E] para esquerda!");
+                        //Console.ReadLine();
+                        break;
+                    }
+                }
+                Console.WriteLine($"\nLocalização do robô:{localizacaoRobo},{direcaoRobo}");
+                Console.WriteLine("-----------------------------------------------------");
+                Console.Write("Faça um movimento: ");
+                Console.ReadLine();
+
+                Console.Clear();
+            }
+
+            static char VirarDireita(char direcaoAtual)
+            {
+                if (direcaoAtual == 'N') return 'L';
+                else if (direcaoAtual == 'L') return 'S';
+                else if (direcaoAtual == 'S') return 'O';
+                else if (direcaoAtual == 'O') return 'N';
+                return direcaoAtual;
+            }
+            static char VirarEsquerda(char direcaoAtual)
+            {
+                if (direcaoAtual == 'N') return 'O';
+                else if (direcaoAtual == 'O') return 'S';
+                else if (direcaoAtual == 'S') return 'L';
+                else if (direcaoAtual == 'L') return 'N';
+                return direcaoAtual;
+            }
+           
+
         }
     }
 }
