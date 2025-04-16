4f2e8cef23eae0ba9a65edcd0cb632c100f71434
tiago <tiagoeweber@gmail.com>
2025-04-15 21:34:57 -0300

    Add project files.

---
+++ Robo.ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Robo.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Robô Tupiniquim");
+            Console.WriteLine("---------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
